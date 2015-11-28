5cc401f4460378d57fe22800139aece447c6b0b1
GETULIO VINICIUS <getuliovinits@gmail.com>
2015-11-28 21:48:45 -0200

    commit #02 - alteracao no arquivo index.php

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ include 'include/geral/php/cabecalho.php';
 				<h1>CMS-Base</h1>
 				<h2>Site em Construção</h2>
 			</hgroup>
-			<p><a href="/cms-base/administracao">Administracao</a></p>
+			<p><a href="/cms-base/administracao">ADMINISTRAÇÃO</a></p>
 			<!-- InstanceEndEditable -->
 		</section>
 		<aside id="conteudoLateral">
